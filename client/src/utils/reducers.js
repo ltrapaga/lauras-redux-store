8aaf6e4716f9809f6bd4d3399f2173246ca79849
Laura Trapaga <trapaga9@gmail.com>
2023-04-11 13:24:43 -0400

    Fix bug in reducers.js

--- client/src/utils/reducers.js
+++ client/src/utils/reducers.js
@@ -11,7 +11,7 @@ import {
 } from './actions';
 
 // The reducer is a function that accepts the current state and an action. It returns a new state based on that action.
-export const reducer = (state, action) => {
+export const reducers = (state, action) => {
   switch (action.type) {
     // Returns a copy of state with an update products array. We use the action.products property and spread it's contents into the new array.
     case UPDATE_PRODUCTS:
@@ -92,4 +92,4 @@ export const reducer = (state, action) => {
   }
 };
 
-export default reducer;
+export default reducers;
