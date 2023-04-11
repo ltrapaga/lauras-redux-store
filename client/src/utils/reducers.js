ce19102ca7ed0378df2920cb32ca1a7f543b4d6d
Laura Trapaga <trapaga9@gmail.com>
2023-04-11 14:35:47 -0400

    Fixing bugs

--- client/src/utils/reducers.js
+++ client/src/utils/reducers.js
@@ -10,8 +10,27 @@ import {
   TOGGLE_CART,
 } from './actions';
 
+const initialState = {
+  products: [],
+  cart: [
+    {
+      _id: '1',
+      name: 'Soup',
+      purchaseQuantity: 1
+    },
+    {
+      _id: '2',
+      name: 'Bread',
+      purchaseQuantity: 2
+    }
+  ],
+  cartOpen: false,
+  categories: [{ name: 'Food' }],
+  currentCategory: '1',
+};
+
 // The reducer is a function that accepts the current state and an action. It returns a new state based on that action.
-export const reducers = (state, action) => {
+export default function reducer(state = initialState, action) {
   switch (action.type) {
     // Returns a copy of state with an update products array. We use the action.products property and spread it's contents into the new array.
     case UPDATE_PRODUCTS:
@@ -91,5 +110,3 @@ export const reducers = (state, action) => {
       return state;
   }
 };
-
-export default reducers;
